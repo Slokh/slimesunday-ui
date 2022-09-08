bbc62bb6764faf1edb571ebf6395f5af3b440565
Kartik <slokh@octane.gg>
2022-09-08 13:57:35 -0400

    Disable rinkeby

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider } = configureChains(
-  [chain.rinkeby, chain.mainnet],
+  [chain.mainnet],
   [publicProvider()]
 );
 const { connectors } = getDefaultWallets({
